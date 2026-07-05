53352af09e866595a18766d67702847556a8cc3c
ROHITH05012005 <ROHITH05012005@users.noreply.github.com>
2026-07-05 08:15:11 +0530

    feat: add admin dashboard button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           {user ? (
             <div className="user-menu">
               <span className="user-name">Hi, {user.name.split(' ')[0]}</span>
-              <Link to="/admin" className="btn-login" style={{ marginLeft: '10px', background: '#333' }}>Dashboard</Link>
+              <Link to="/admin" className="btn-admin" style={{ fontSize: '0.9rem', color: 'var(--primary)', textDecoration: 'underline', marginLeft: '10px' }}>Dashboard</Link>
               <button className="btn-logout" onClick={logout} style={{ marginLeft: '10px' }}>Logout</button>
             </div>
           ) : (
